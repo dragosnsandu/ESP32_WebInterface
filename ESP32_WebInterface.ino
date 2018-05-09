5a42c0db3efe38ca22c41831df08854417cb3b54
Dragos Sandu <dragos.n.sandu@gmail.com>
2018-05-09 20:37:41 +0300

    Clear comments

--- ESP32_WebInterface.ino
+++ ESP32_WebInterface.ino
@@ -1,16 +1,6 @@
 #include <FastLED.h>
-//#include <ESP32WebServer.h>
 #include <WiFi.h>
 #include "esp_system.h"
-
-//String getMacAddress() {
-//	uint8_t baseMac[6];
-//	// Get MAC address for WiFi station
-//	esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-//	char baseMacChr[18] = { 0 };
-//	sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-//	return String(baseMacChr);
-//}
 
 const char* ssid = "*****";
 const char* password = "*****";
@@ -21,53 +11,15 @@ const char* privateKey = "....................";
 #define DATA_PIN 3
 #define NUM_LEDS 16
 CRGB leds[NUM_LEDS];
-//byte mac[6];
 
 unsigned int brightness = 10;
 unsigned int numberofLEDs = 16;
 
-//ESP32WebServer server(80);
 WiFiClient client;
-
-
-//void handleRoot()
-//{
-//	server.send(200, "text/plain", "Hai noroc, sa vedem daca merge!");
-//	//server.send(200, "text/html", "/index.html");
-//}
-
-//void handleSettings()
-//{
-//	//digitalWrite(pinLED, !digitalRead(pinLED));
-//	//server.send(204, "");
-//	//if (server.arg("Brightness") == "")
-//	//{
-//	//	server.send(200, "text/html", "Nu am gasit parametrul corect");
-//	//}
-//	else
-//	{
-//		//Serial.println(brightness);
-//		brightness = server.arg("Brightness").toInt();
-//
-//		FastLED.setBrightness(brightness);
-//
-//		for (int i = 0; i < NUM_LEDS; i++)
-//		{
-//			leds[i] = CRGB::White;
-//			FastLED.show();
-//		}
-//
-//		//Serial.println("Brightnessafter:" + brightness);
-//		server.send(200, "text/plain", "OK");
-//	}
-//	//server.send(204, "");
-//
-//}
 
 void setup()
 {
 	FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-	//pinMode(PixelPin, OUTPUT);
 	Serial.begin(115200);
 	WiFi.begin(ssid, password);
 	Serial.println("");
@@ -83,17 +35,6 @@ void setup()
 	Serial.print("IP address: ");
 	Serial.println(WiFi.localIP());
 
-	//Serial.println(getMacAddress());
-
-	//server.on("/", handleRoot);
-	//server.on("/settings", handleSettings);
-
-	//server.begin();
-
-	//strip.Begin();
-	//strip.SetBrightness(10);
-	//strip.Show();
-	//Serial.println(strip.GetBrightness());
 	FastLED.setBrightness(brightness);
 
 	for (int i = 0; i < NUM_LEDS; i++)
@@ -140,8 +81,6 @@ void loop()
 		Serial.print(line);
 		if (lineNumber == 15)
 		{
-			//Serial.println("a intrat");
-
 			char lineContent[20];
 			char * token;
 			char BrightnessValue[4];
@@ -154,14 +93,9 @@ void loop()
 			brightness = atoi(BrightnessValue);
 
 			token = strtok(NULL, ",");
-			//token = strtok(lineContent, ",");
-			//token = strtok(NULL, ",");
 			strcpy(NoLValue,token);
 			numberofLEDs = atoi(NoLValue);
-			//Serial.println("TEST");
-			//Serial.println(numberofLEDs);
 
-			//brightness = line.toInt();
 			FastLED.setBrightness(brightness);
 
 			for (int i = 0; i < numberofLEDs; i++)
@@ -175,9 +109,6 @@ void loop()
 				leds[i] = CRGB::Black;
 				FastLED.show();
 			}
-
-
-			//Serial.println(line.toInt());
 		}
 		lineNumber++;
 	}
